baa18ace8caf97087445ea1eb1dc75727d032d3b
Tom Banish <tombanish@gmail.com>
2020-03-31 21:22:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,9 @@ class Song
   def self.new_by_filename(filename)
     binding.pry
     split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_new(split_name[0])
+    song.artist = artist
   end
   
   def artist_name=(name)
